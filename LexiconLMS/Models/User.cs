3e8b1af4d97557e0632a10baba50220be9424834
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-06-05 14:40:11 +0200

    init1

---
+++ LexiconLMS/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.Models
+{
+    public class User
+    {
+        
+        public int Id { get; set; }
+        public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return (($"{FirstName} {LastName}")); } }
+        public string EMail { get; set; }
+    }
+}
