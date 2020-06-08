b4bf53f0e59ad6ee660b2e23a8cbefaf2b159861
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-06-08 11:54:14 +0200

    0608_init1

---
+++ LexiconLMS/Data/SeedData.cs
@@ -0,0 +1,75 @@
+﻿using LexiconLMS.Data;
+using LexiconLMS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace LexiconLMS
+{
+    public static class SeedData
+    {
+        public static async System.Threading.Tasks.Task InitializeAsync(IServiceProvider services, string adminPW)
+        {
+            var options = services.GetRequiredService<DbContextOptions<ApplicationDbContext>>();
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                //Skapa managers för att hantera Users och Roles
+                var userManager = services.GetRequiredService<UserManager<User>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                var roleNames = new[] { "Admin", "Member" };
+
+                foreach (var name in roleNames)
+                {
+                    //Om rollen finns fortsätt med nästa i "listan"
+                    if (await roleManager.RoleExistsAsync(name)) continue;
+
+                    //Annars skapa rollen
+                    var role = new IdentityRole { Name = name };
+                    var result = await roleManager.CreateAsync(role);
+
+                    //Om nåt går fel kasta en exception
+                    if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
+                }
+
+                var adminEmail = "admin@gym.se";
+
+                var foundUser = await userManager.FindByEmailAsync(adminEmail);
+
+                if (foundUser != null) return;
+
+                var user = new User
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    FirstName = "Admin",
+                    LastName = "Gym",
+                    //TimeOfRegistration = DateTime.Now
+                };
+
+                var addUserResult = await userManager.CreateAsync(user, adminPW);
+
+                if (!addUserResult.Succeeded) throw new Exception(string.Join("\n", addUserResult.Errors));
+
+
+                var adminUser = await userManager.FindByNameAsync(adminEmail);
+
+                foreach (var role in roleNames)
+                {
+                    if (await userManager.IsInRoleAsync(adminUser, role)) continue;
+
+                    var addToRoleResult = await userManager.AddToRoleAsync(adminUser, role);
+
+                    if (!addToRoleResult.Succeeded) throw new Exception(string.Join("\n", addToRoleResult.Errors));
+                }
+
+
+
+
+            }
+        }
+    }
+}
---
+++ LexiconLMS/Migrations/20200608082751_0608a.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LexiconLMS.Migrations
+{
+    public partial class _0608a : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
