c7563ae93e1e94dcd72b1357800377e5cdaf3919
Jörgen Forsberg <j.forsberg65@outlook.com>
2020-06-09 15:23:45 +0200

    Login Admin/Menber redirect

--- LexiconLMS/Models/Course.cs
+++ LexiconLMS/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace LexiconLMS.Models
     public class Course
     {
         public int Id { get; set; }
+
+        [Display(Name = "Namn")]
         public string Name { get; set; }
+
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+        
+        [Display(Name = "Start")]
         public DateTime StartDate { get; set; } 
         
         public ICollection<User> Users { get; set; }
