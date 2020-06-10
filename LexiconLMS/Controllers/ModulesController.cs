c1a6ee155a86bdccaa2faee35c509c5ef6265c48
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-06-10 15:42:49 +0200

    add/edit module

--- LexiconLMS/Controllers/ModulesController.cs
+++ LexiconLMS/Controllers/ModulesController.cs
@@ -49,7 +49,7 @@ namespace LexiconLMS.Controllers
         // GET: Modules/Create
         public IActionResult Create()
         {
-            ViewData["Courses"] = new SelectList(_context.Courses, "Name", "Name");
+            ViewData["Courses"] = new SelectList(_context.Courses, "Id", "Name");
             return View();
         }
 
@@ -66,7 +66,7 @@ namespace LexiconLMS.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Id", @module.CourseId);
+            ViewData["Courses"] = new SelectList(_context.Courses, "Id", "Name");
             return View(@module);
         }
 
@@ -83,7 +83,9 @@ namespace LexiconLMS.Controllers
             {
                 return NotFound();
             }
-            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Id", @module.CourseId);
+            //ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Id", @module.CourseId);
+            ViewData["Courses"] = new SelectList(_context.Courses, "Id", "Name");
+
             return View(@module);
         }
 
@@ -119,7 +121,7 @@ namespace LexiconLMS.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Id", @module.CourseId);
+            ViewData["Courses"] = new SelectList(_context.Courses, "Id", "Name");
             return View(@module);
         }
 
---
+++ LexiconLMS/Migrations/20200610113358_brainlost2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LexiconLMS.Migrations
+{
+    public partial class brainlost2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StartDate",
+                value: new DateTime(2020, 6, 10, 13, 33, 57, 652, DateTimeKind.Local).AddTicks(8204));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTime(2020, 6, 10, 13, 33, 57, 656, DateTimeKind.Local).AddTicks(9522), new DateTime(2020, 6, 10, 13, 33, 57, 656, DateTimeKind.Local).AddTicks(8652) });
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTime(2020, 6, 10, 13, 33, 57, 657, DateTimeKind.Local).AddTicks(4726), new DateTime(2020, 6, 10, 13, 33, 57, 657, DateTimeKind.Local).AddTicks(3934) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StartDate",
+                value: new DateTime(2020, 6, 10, 10, 28, 46, 403, DateTimeKind.Local).AddTicks(6866));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTime(2020, 6, 10, 10, 28, 46, 412, DateTimeKind.Local).AddTicks(9304), new DateTime(2020, 6, 10, 10, 28, 46, 412, DateTimeKind.Local).AddTicks(8142) });
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTime(2020, 6, 10, 10, 28, 46, 413, DateTimeKind.Local).AddTicks(6190), new DateTime(2020, 6, 10, 10, 28, 46, 413, DateTimeKind.Local).AddTicks(5125) });
+        }
+    }
+}
