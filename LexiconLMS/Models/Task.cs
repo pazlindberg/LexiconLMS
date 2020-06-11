b42f669d7d7f3ddff679b829fac50bf22ee427db
k-sten <stenen@gmail.com>
2020-06-11 09:41:02 +0200

    Formatering av task model + views

--- LexiconLMS/Models/Task.cs
+++ LexiconLMS/Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,15 +11,31 @@ namespace LexiconLMS.Models
     {
         public int Id { get; set; }
         //public string Type { get; set; }
-
+        [Display(Name = "Namn")]
         public string Name { get; set; }
+
+        [Display(Name = "Start")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm}")]
         public DateTime StartDate { get; set; }
+
+        [Display(Name = "Slut")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm}")]
         public DateTime EndDate { get; set; }
 
+
+        [Display(Name = "Modul")]
         public int ModuleId { get; set; }
+
+        [Display(Name = "Aktivitet")]
         public int TaskTypeId { get; set; }
 
+
+        [Display(Name = "Modul")]
         public Module Module { get; set; }
+
+        [Display(Name = "Aktivitet")]
         public TaskType TaskType { get; set; }
 
     }
